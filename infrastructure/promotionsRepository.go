86160bb4d231648e574303c61d58ce6c309cc620
Smbat A <37016429+Smbat-A@users.noreply.github.com>
2023-04-11 03:17:56 +0400

    Update promotionsRepository.go

--- infrastructure/promotionsRepository.go
+++ infrastructure/promotionsRepository.go
@@ -19,7 +19,7 @@ type Promotion struct {
 func InitDataLayer() *mongo.Client {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:30001"))
 	if err != nil {
 		log.Fatal(err)
 	} else {
